3224f3005f26021064157298853d2ea9d5d53143
kaban <silver-ralf@mail.ru>
2023-05-29 22:09:11 +0500

    29.05.23

--- NewBD/NewBD.py
+++ NewBD/NewBD.py
@@ -1,32 +1,40 @@
-import customtkinter as ctk
+import customtkinter
+import tkinter
 
 
-class MainForm(ctk.CTk):
+class MainForm(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("600x500")
+        self.geometry("368x480")
         self.title("Создание новой БД ПК'Аптека-Урал'")
 
-        self.label_one = ctk.CTkLabel(self, text='Очищаем БД:')
-        self.label_one.grid(row=0, column=0, padx=20, pady=10)
-        check_var = ctk.StringVar(value="on")
-        check_off = ctk.StringVar(value="off")
-        self.checkbox_all = ctk.CTkCheckBox(self, text='Удаляем все данные,\n кроме справочникво', variable=check_var,
-                                            onvalue="on", offvalue="off", command=self.check_ext)
-        self.checkbox_all.grid(row=1, column=0, padx=20, pady=0)
 
-        self.checkbox_one = ctk.CTkCheckBox(self, text='Наличие переносим в один документ, \n остальные удаялем',
-                                            variable=check_off, onvalue="on", offvalue="off", command=self.check_ext)
-        self.checkbox_one.grid(row=1, column=1, padx=20, pady=0)
-        self.button = ctk.CTkButton(self, command=self.button_click)
+        # список для выбора действий
+        self.radiobutton_frame = customtkinter.CTkFrame(self)
+        self.radiobutton_frame.grid(row=0, column=0, padx=(10, 10), pady=(10, 0), sticky="nsew",)
+        self.radio_var = tkinter.IntVar(value=0)
+        self.label_radio_group = customtkinter.CTkLabel(master=self.radiobutton_frame, text="Выберите действие:")
+        self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
+
+        self.radio_button_all_delete = customtkinter.CTkRadioButton(master=self.radiobutton_frame,
+                                                                    variable=self.radio_var,
+                                                                    value=0,
+                                                                    text="Удалить все данные, справочники остаются")
+        self.radio_button_all_delete.grid(row=1, column=2, pady=10, padx=20, sticky="n")
+        self.radio_button_not_nalic = customtkinter.CTkRadioButton(master=self.radiobutton_frame,
+                                                                   variable=self.radio_var,
+                                                                   value=1,
+                                                                   text="Удалить все двжиения товара,\nналичие перенести в документ ввод остатков")
+        self.radio_button_not_nalic.grid(row=2, column=2, pady=10, padx=20, sticky="n")
+
+        self.button = customtkinter.CTkButton(self, command=self.run_clear_bd)
         self.button.grid(row=2, column=0, padx=40, pady=10)
 
-    def check_ext(self):
-        if self.checkbox_all.get() == "off":
-            self.checkbox_one.setvar(value="on")
-        # add methods to app
-
-    def button_click(self):
+        self.label_one = customtkinter.CTkLabel(self, text='Очищаем БД:')
+        self.label_one.grid(row=3, column=0, padx=20, pady=10)
+    #
+    def run_clear_bd(self):
+        # if self.radio_button_not_nalic.cget()
         print("button click")
         print("213")
 
---
+++ Service/fdb.py
@@ -0,0 +1,11 @@
+import fdb
+
+
+def get_setting_bd():
+    pass
+
+def connect_fdb(setting):
+    pass
+
+def disconnect_fdb(con):
+    pass
